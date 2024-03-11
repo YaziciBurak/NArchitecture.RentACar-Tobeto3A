1f8498c4adcc79b8a39a3609fa025f180bb7df39
Burak Yazıcı <133709862+YaziciBurak@users.noreply.github.com>
2024-03-12 02:05:45 +0300

    Add project files.

---
+++ src/webProjects/Application/Features/Brands/Profiles/MappingBrandProfiles.cs
@@ -0,0 +1,26 @@
+﻿using Application.Features.Brands.Commands.Create;
+using Application.Features.Brands.Commands.Delete;
+using Application.Features.Brands.Commands.Update;
+using Application.Features.Brands.Dtos;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Features.Brands.Profiles;
+
+public class MappingBrandProfiles:Profile
+{
+    public MappingBrandProfiles()
+    {
+        CreateMap<Brand, CreateBrandCommand>().ReverseMap();
+        CreateMap<Brand, DeleteBrandCommand>().ReverseMap();
+        CreateMap<Brand, UpdateBrandCommand>().ReverseMap();
+
+        CreateMap<Brand, CreatedBrandResponse>().ReverseMap();
+        CreateMap<Brand, GetByIdBrandResponse>().ReverseMap();
+        CreateMap<Brand, GetListedBrandResponse>().ReverseMap();
+        CreateMap<Brand, DeletedBrandResponse>().ReverseMap();
+        CreateMap<Brand, UpdatedBrandResponse>().ReverseMap();
+
+
+    }
+}
---
+++ src/webProjects/Application/Features/Models/Commands/Create/CreateModelCommand.cs
@@ -0,0 +1,13 @@
+﻿using Application.Features.Brands.Dtos;
+using Application.Features.Cars.Dtos;
+using Application.Features.Models.Dtos;
+using MediatR;
+
+namespace Application.Features.Models.Commands.Create;
+
+public class CreateModelCommand : IRequest<CreatedModelResponse>
+{
+    public int BrandId { get; set; }
+    public string Name { get; set; }
+
+}
